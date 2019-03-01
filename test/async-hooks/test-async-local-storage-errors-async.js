b571f876fe9aac4f7f4e72e7835ac9e54e74220f
vdeturckheim <vlad2t@hotmail.com>
2019-03-01 21:31:36 +0100

    async_hooks: introduce async-context API
    Adding AsyncLocalStorage class to async_hooks
     module.
    This API provide a simple CLS-like set
    of features.
    
    Co-authored-by: Andrey Pechkurov <apechkurov@gmail.com>

---
+++ test/async-hooks/test-async-local-storage-errors-async.js
@@ -0,0 +1,27 @@
+'use strict';
+require('../common');
+const assert = require('assert');
+const { AsyncLocalStorage } = require('async_hooks');
+
+// case 1 fully async APIS (safe)
+const asyncLocalStorage = new AsyncLocalStorage();
+
+let i = 0;
+process.setUncaughtExceptionCaptureCallback((err) => {
+  ++i;
+  assert.strictEqual(err.message, 'err' + i);
+  assert.strictEqual(asyncLocalStorage.getStore().get('hello'), 'node');
+});
+
+asyncLocalStorage.run((str) => {
+  const store = asyncLocalStorage.getStore();
+  assert.strictEqual(str, 'hello');
+  store.set('hello', 'node');
+  setTimeout(() => {
+    process.nextTick(() => {
+      assert.strictEqual(i, 2);
+    });
+    throw new Error('err2');
+  }, 0);
+  throw new Error('err1');
+}, 'hello');
