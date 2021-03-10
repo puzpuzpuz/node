8efa7c3b0ecbfd81a8fcaba297b0078c4659fb43
Andrey Pechkurov <apechkurov@gmail.com>
2021-03-10 15:34:09 +0300

    Experiment with fsPromises.readFile

--- benchmark/fs/readfile.js
+++ benchmark/fs/readfile.js
@@ -1,11 +1,12 @@
-// Call fs.readFile over and over again really fast.
+// Call fs.readFile/fsPromises.readFile over and over again really fast.
 // Then see how many times it got called.
-// Yes, this is a silly benchmark.  Most benchmarks are silly.
+// Yes, this is a silly benchmark. Most benchmarks are silly.
 'use strict';
 
 const path = require('path');
 const common = require('../common.js');
 const fs = require('fs');
+const fsPromises = require('fs').promises;
 const assert = require('assert');
 
 const tmpdir = require('../../test/common/tmpdir');
@@ -16,10 +17,11 @@ const filename = path.resolve(tmpdir.path,
 const bench = common.createBenchmark(main, {
   duration: [5],
   len: [1024, 16 * 1024 * 1024],
-  concurrent: [1, 10]
+  concurrent: [1, 10],
+  type: ['callback', 'promise'],
 });
 
-function main({ len, duration, concurrent }) {
+function main({ len, duration, concurrent, type }) {
   try { fs.unlinkSync(filename); } catch {}
   let data = Buffer.alloc(len, 'x');
   fs.writeFileSync(filename, data);
@@ -35,8 +37,22 @@ function main({ len, duration, concurrent }) {
     process.exit(0);
   }, duration * 1000);
 
-  function read() {
-    fs.readFile(filename, afterRead);
+  let read;
+  switch (type) {
+    case 'callback':
+      read = function () {
+        fs.readFile(filename, afterRead);
+      };
+      break;
+    case 'promise':
+      read = function () {
+        fsPromises.readFile(filename)
+          .then((data) => afterRead(null, data))
+          .catch(afterRead);
+      };
+      break;
+    default:
+      throw new Error(`invalid type: ${type}`);
   }
 
   function afterRead(er, data) {
